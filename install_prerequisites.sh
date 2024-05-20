38fe7281e3a7b68edb3c1da41cffc643a333d09b
ilmodderd <98211769+ilmodderd@users.noreply.github.com>
2024-05-20 21:06:07 +0200

    Add files via upload

---
+++ install_prerequisites.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Installa colorama
+pip3 install colorama
+
+# Installa Metasploit Framework
+apt update
+apt install -y metasploit-framework
+
+# Installa Apache2
+apt install -y apache2
